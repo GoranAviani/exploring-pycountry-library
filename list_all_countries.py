8cc56b5b2fb0d5f5f4fc0ce26434db765fb1222b
goranaviani <goranaviani@gmail.com>
2020-05-29 17:37:00 +0200

    listing raw country data

--- list_all_countries.py
+++ list_all_countries.py
@@ -4,9 +4,9 @@ def main():
     list_all()
 
 def list_all():
-    all_countries = pycountry.countries
-    for country_no in range[0, len(pycountry.countries)]:
-        print(all_countries[country_no])
+    all_countries = list(pycountry.countries)
+    for country_no in range(0, len(all_countries)):
+        print(str(all_countries[country_no]))
 
 
 if __name__=="__main__":
