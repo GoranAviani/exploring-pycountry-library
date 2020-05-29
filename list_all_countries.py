f2c7a829e7dfc8e0c357c10486b84e4afaea2182
goranaviani <goranaviani@gmail.com>
2020-05-29 20:54:33 +0200

    wip

--- list_all_countries.py
+++ list_all_countries.py
@@ -1,15 +1,16 @@
 import pycountry
 
 def searching_by_codes(code):
+
     code_length = len(code)
     if code_length == 2:
         country_info = pycountry.countries.get(alpha_2=code)
-        print("{}".format(country_info.name)
+        print(country_info.name)
     elif code_length == 3:
         country_info = pycountry.countries.get(alpha_3=code)
-        print("{}".format(country_info.name)
+        print(country_info.name)
     else:
-        print("This is not a country code")
+        print("This is not a alpha 2 or 3 country code")
 
 def searching_by_alpha_2(alpha_2_code):
 
@@ -23,7 +24,7 @@ def main():
     list_all()
     list_all_historic_countries()
     searching_by_alpha_2("1SE")
-    searching_by_codes("SE")
+    searching_by_codes("SWE")
 
 def list_all_historic_countries():
     all_countries = list(pycountry.historic_countries)
