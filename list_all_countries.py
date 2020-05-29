ff9058f2895a22ab2ad74b0d8b7c38d5376f8b1e
goranaviani <goranaviani@gmail.com>
2020-05-29 17:19:20 +0200

    wip

--- list_all_countries.py
+++ list_all_countries.py
@@ -1,8 +1,13 @@
 import pycountry
 
+def main():
+    list_all()
+
 def list_all():
     all_countries = pycountry.countries
     for country_no in range[0, len(pycountry.countries)]:
-        pass
-       # print([country_no])
+        print(all_countries[country_no])
 
+
+if __name__=="__main__":
+    main()
