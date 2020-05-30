27e6722e03440c5c26546c41fe9419955b39e153
goranaviani <goranaviani@gmail.com>
2020-05-30 08:56:45 +0200

    Calling searching functions from search module

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from list_all_countries import (
     list_all,
     list_all_historic_countries,
-    searching_by_alpha_2,
-    searching_by_codes
 )
+from search_countries import (
+    searching_by_alpha_2,
+    searching_by_alpha_codes
+)
+
 
 def main():
     list_all()
     list_all_historic_countries()
     searching_by_alpha_2("1SE")
-    searching_by_codes("SWE")
+    searching_by_alpha_codes("SWE")
 
 if __name__=="__main__":
     main()
