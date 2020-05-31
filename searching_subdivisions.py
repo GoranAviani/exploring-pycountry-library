5c60ac6cddf84e7169ef08ac9b56d3a5e5669940
goranaviani <goranaviani@gmail.com>
2020-05-31 18:25:19 +0200

    wip

---
+++ searching_subdivisions.py
@@ -0,0 +1,15 @@
+import pycountry
+
+def search_by_subdivision(subdivision):
+    """
+   There’s also a “fuzzy” search to help people discover “proper” countries
+    for names that might only actually be subdivisions. The fuzziness also
+    includes normalizing unicode accents. There’s also a bit of prioritization
+     included to prefer matches on country names before subdivision names
+     and have countries with more matches be listed before ones with fewer
+      matches:
+    :param subdivision: String containing a subdivision country name
+    :return: n/a
+    """
+    country_info = pycountry.countries.search_fuzzy(subdivision)
+    print(str(country_info))
