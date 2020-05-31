affc40a85b3e8252e02e24aa98d3c254b4fb7720
goranaviani <goranaviani@gmail.com>
2020-05-31 18:41:08 +0200

    list all currencies

--- currency.py
+++ currency.py
@@ -3,6 +3,5 @@ import pycountry
 def list_all_currencies():
     all_currencies = pycountry.currencies
     for currency in all_currencies:
-        print(str(currency))
-        #print("{}, {}".format(found_country.alpha_2, found_country.name))
+        print("{}, {}".format(currency.alpha_3, currency.name))
 
